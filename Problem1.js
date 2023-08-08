244e8f749bec694cebd801f6b2cd8e15aada1393
devmashum <dev.mashum@gmail.com>
2023-08-08 23:01:25 +0200

    new commit

--- Problem1.js
+++ Problem1.js
@@ -4,3 +4,8 @@ var array = [1, 2, 3, 4, 5];
 array[array.length - 1] = 6;
 
 console.log(array);
+
+
+
+git remote add origin https://github.com/devmashum/4th_assignment.git
+git push - u origin main
