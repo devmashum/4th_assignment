8e75fa45a2fc60356bb88d6706fec9d1dd3db5b9
devmashum <dev.mashum@gmail.com>
2023-08-09 01:32:12 +0200

    new commit

--- Problem3.js
+++ Problem3.js
@@ -1,1 +1,33 @@
 // Problem-3:
+
+function sortMaker(arr) {
+
+    if (arr.some(num => num < 0)) {
+        return "Invalid Input";
+    }
+
+    if (arr[0] === arr[1]) {
+        return "equal"
+    }
+
+    return arr.slice().sort(function (a, b) {
+        return b - a;
+    });
+}
+
+var input1 = [2, 3];
+var result1 = sortMaker(input1);
+var input2 = [4, 2];
+var result2 = sortMaker(input2);
+var input3 = [4, 4];
+var result3 = sortMaker(input3);
+var input4 = [1, 2];
+var result4 = sortMaker(input4);
+var input5 = [4, -2];
+var result5 = sortMaker(input5);
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5); 
--- Problem4.js
+++ Problem4.js
@@ -1,1 +1,28 @@
 // Problem-4:
+
+function sortMaker(arr) {
+    // Check for negative numbers in the array
+    if (arr.some(num => num < 0)) {
+        return "Invalid";
+    }
+    if (arr[0] === arr[1]) {
+        return "equal"
+    }
+    // Sort the array
+    return arr.slice().sort(function (a, b) {
+        return b - a; // Sort in descending order
+    });
+}
+
+var unsortedArray1 = [3, -6]; // Contains a negative number
+var result1 = sortMaker(unsortedArray1);
+
+var unsortedArray2 = [4, 2]; // All positive numbers
+var result2 = sortMaker(unsortedArray2);
+
+var unsortedArray3 = [4, 4]; // All positive numbers
+var result3 = sortMaker(unsortedArray3);
+
+console.log(result1); // Output: Invalid
+console.log(result2); // Output: [8, 6, 4, 2, 0]
+console.log(result3); // Output: [8, 6, 4, 2, 0]
