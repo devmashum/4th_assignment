3cadbeccb5930362dd1ab523dfbaf7c61cd3683e
devmashum <dev.mashum@gmail.com>
2023-08-09 10:56:32 +0200

    new commit

--- Problem4.js
+++ Problem4.js
@@ -1,28 +1,15 @@
 // Problem-4:
+function findAddress(obj) {
+    const address = {
+        street: 10,
+        house: "15A",
+        society: "Earth Perfect"
+    };
 
-function sortMaker(arr) {
-    // Check for negative numbers in the array
-    if (arr.some(num => num < 0)) {
-        return "Invalid";
-    }
-    if (arr[0] === arr[1]) {
-        return "equal"
-    }
-    // Sort the array
-    return arr.slice().sort(function (a, b) {
-        return b - a; // Sort in descending order
-    });
+    const values = Object.values(address);
+    return values;
 }
 
-var unsortedArray1 = [3, -6]; // Contains a negative number
-var result1 = sortMaker(unsortedArray1);
+var result = findAddress();
 
-var unsortedArray2 = [4, 2]; // All positive numbers
-var result2 = sortMaker(unsortedArray2);
-
-var unsortedArray3 = [4, 4]; // All positive numbers
-var result3 = sortMaker(unsortedArray3);
-
-console.log(result1); // Output: Invalid
-console.log(result2); // Output: [8, 6, 4, 2, 0]
-console.log(result3); // Output: [8, 6, 4, 2, 0]
+console.log(result);
